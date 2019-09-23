33237a084a00909d1df3bd4ab68ac8e59f92af8b
ADC04 <allendcooper@gmail.com>
2019-09-23 15:26:08 -0400

    reformatted photos as background images of div with fixed dimensions, with 'background-size: cover' and 'background-repeat: no repeat' for neat tile effect

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,13 +2,29 @@ import React from "react";
 
 export function CardList({ children }) {
     return (
-      <div className="card-deck">
+      <div className="container">
           {children}
       </div>
     );
   }
   
   export function CardListItem(props) {
-    return <div className="card"><img src={props.src} key={props.key} className="card-img-top" alt="..."/></div>
+      const styleImg={
+        position: "relative",
+        float: "left",
+        width:  "200px",
+        height: "300px",
+        margin: "10px",
+        backgroundPosition: "50% 50%",
+        backgroundRepeat: "no-repeat",
+        borderWidth: "2px",
+        borderStyle: "solid",
+        borderColor: "black",
+        backgroundSize: "cover",
+        backgroundImage: "url(" + props.src + ")"
+      }
+    return <div style={styleImg}>
+        {/* <img src={props.src} key={props.key} className="card-img-top img-fluid" alt="..."/> */}
+      </div>
   }
 
