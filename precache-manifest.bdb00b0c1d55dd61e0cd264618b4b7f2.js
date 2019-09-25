42018cc106e7f869ac09f587d5b472dca64a4a5b
ADC04 <allendcooper@gmail.com>
2019-09-24 20:36:14 -0400

    Updates

---
+++ precache-manifest.bdb00b0c1d55dd61e0cd264618b4b7f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6c6417778e062e89b79a92a44f1b79",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "be43e37e855cb2f36ca2",
+    "url": "/ClickyGame/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6795b891c917c3ec7fb7",
+    "url": "/ClickyGame/static/js/2.dc6a231d.chunk.js"
+  },
+  {
+    "revision": "be43e37e855cb2f36ca2",
+    "url": "/ClickyGame/static/js/main.dd81f472.chunk.js"
+  },
+  {
+    "revision": "33dfbbd81437a1f4f82e",
+    "url": "/ClickyGame/static/js/runtime~main.50b0c78c.js"
+  }
+]);
